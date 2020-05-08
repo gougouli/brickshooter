68d9936d8e1e1ead7a411c497c3edc0c2269ff15
Greg <gougouli69@outlook>
2020-05-08 13:15:02 +0200

    Add shop file

--- shop.py
+++ shop.py
@@ -1,29 +1,30 @@
-def readFile():
-    try:
-        myFile = open("data.txt", "r")
-        file = myFile.read()
-        myFile.close()
-        return file
-    except:
-        print("Erreur avec l'ouverture du fichier")
+def add(line):
+    values =readFile()
+    currentValue = int(getValueLine(values, line))
+    price = currentValue**2
+    money = int(getValueLine(values, 1))
+    if money >= price:
+        moneyRestant = money - price
+        lvl = currentValue + 1
+        writeFile(1, moneyRestant)
+        writeFile(line, lvl)
+        print("Acheté")
+        return 1
+    print("Pas d'argent")
+    return 0
 
-def getValueLine(value, line):
-    valeur = value.split("\n")
-    return valeur[line]
-
-def writeFile(line, valeur):
-    value = readFile()
-    myTblo = []
-    myTblo.append(getValueLine(value, 0))
-    myTblo.append(getValueLine(value, 1))
-    myTblo.append(getValueLine(value, 2))
-    myTblo.append(getValueLine(value, 3))
-    myTblo.append(getValueLine(value, 4))
-    file = open("data.txt", "w")
-    for i in range(0,5):
-        if i != line:
-            file.write(str(myTblo[i]))
-            file.write("\n")
-        else:
-            file.write(str(valeur))
-
+def remove(line):
+    values = readFile()
+    currentValue = int(getValueLine(values, line))
+    returnPrice = currentValue * 2
+    money = int(getValueLine(values, 1))
+    if currentValue > 0:
+        newMoney = money + returnPrice
+        lvl = currentValue - 1
+        writeFile(1, newMoney)
+        writeFile(line, lvl)
+        print("Niveau retiré")
+        return 1
+    print("Vous etes niveau 0 ! Attention")
+    return 0
+remove(move_speed)
