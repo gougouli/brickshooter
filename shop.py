cc2cc66e94ebe449d2c9637ccd2893342ff7950b
Greg <gougouli69@outlook>
2020-05-08 12:02:10 +0200

    Shop fonctionnal

--- shop.py
+++ shop.py
@@ -1,3 +1,9 @@
+highscore = 0
+money = 1
+defil_speed = 2
+shoot_speed = 3
+move_speed = 4
+
 def readFile():
     try:
         myFile = open("data.txt", "r")
@@ -14,11 +20,11 @@ def getValueLine(value, line):
 def writeFile(line, valeur):
     value = readFile()
     myTblo = []
-    myTblo.append(getValueLine(value, 0))
-    myTblo.append(getValueLine(value, 1))
-    myTblo.append(getValueLine(value, 2))
-    myTblo.append(getValueLine(value, 3))
-    myTblo.append(getValueLine(value, 4))
+    myTblo.append(getValueLine(value, highscore))
+    myTblo.append(getValueLine(value, money))
+    myTblo.append(getValueLine(value, defil_speed))
+    myTblo.append(getValueLine(value, shoot_speed))
+    myTblo.append(getValueLine(value, move_speed))
     file = open("data.txt", "w")
     for i in range(0,5):
         if i != line:
@@ -26,4 +32,35 @@ def writeFile(line, valeur):
             file.write("\n")
         else:
             file.write(str(valeur))
+            file.write("\n")
 
+def add(line):
+    values =readFile()
+    currentValue = int(getValueLine(values, line))
+    price = currentValue**2
+    money = int(getValueLine(values, 1))
+    if money >= price:
+        moneyRestant = money - price
+        lvl = currentValue + 1
+        writeFile(1, moneyRestant)
+        writeFile(line, lvl)
+        print("Acheté")
+        return 1
+    print("Pas d'argent")
+    return 0
+
+def remove(line):
+    values = readFile()
+    currentValue = int(getValueLine(values, line))
+    returnPrice = currentValue * 2
+    money = int(getValueLine(values, 1))
+    if currentValue > 0:
+        newMoney = money + returnPrice
+        lvl = currentValue - 1
+        writeFile(1, newMoney)
+        writeFile(line, lvl)
+        print("Niveau retiré")
+        return 1
+    print("Vous etes niveau 0 ! Attention")
+    return 0
+remove(move_speed)
